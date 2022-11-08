0baad09e2d46f1d8c77a028f49a95bbb7984e1e6
Hadrien <verrier.hadrien.pro@gmail.com>
2022-11-08 10:08:17 +0100

    add neovim

--- projects/music.py
+++ projects/music.py
@@ -1,2 +1,9 @@
 from sardine import *
 
+S('bd', amp=2).out()
+
+# @swim # replace me by 'die'
+# def bd(d=1):
+#     """Loud bassdrum"""
+#     S('bd', amp=2).out()
+#     again(bd, d=1) # again == anew == cs
